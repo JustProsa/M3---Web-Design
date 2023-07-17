60ec3bd5fd7d6b7254ec3c59645712217272971e
JustProsa <90556911+JustProsa@users.noreply.github.com>
2023-07-17 23:23:03 +0200

    M3-D7-D8

--- M3-D7-D8/script.js
+++ M3-D7-D8/script.js
@@ -24,16 +24,23 @@ console.log(arrowsRight);
 for (let i = 0; i < movieRows.length; i++) {
   arrowsLeft[i].addEventListener("click", function () {
     let currentPosition = parseInt(movieRows[i].style.left) || 0;
-    let newPosition = currentPosition + 100; // Sposta l'elemento di 100px verso destra
-    movieRows[i].style.left = newPosition + "px";
-    console.log(newPosition);
+    let newPosition = currentPosition + 10; // Sposta l'elemento di 100px verso destra
+
+    // Verifica se la nuova posizione è maggiore di 0 (limite sinistro)
+    if (newPosition <= 0) {
+      movieRows[i].style.left = newPosition + "%";
+      console.log(newPosition);
+    }
   });
 
   arrowsRight[i].addEventListener("click", function () {
     let currentPosition = parseInt(movieRows[i].style.left) || 0;
-    let newPosition = currentPosition - 100; // Sposta l'elemento di 100px verso sinistra
-    movieRows[i].style.left = newPosition + "px";
-    console.log(newPosition);
+    let newPosition = currentPosition - 10; // Sposta l'elemento di 100px verso sinistra
+    // Verifica se la nuova posizione è minore o uguale a 0 (limite destro)
+    if (newPosition >= -130) {
+      movieRows[i].style.left = newPosition + "%";
+      console.log(newPosition);
+    }
   });
 }
 
